9062d02d74484be52d7ded5f8228b44430ef2d7a
Thomas Moran <thomaslmoran.14@gmail.com>
2015-10-13 17:06:40 +0100

    Test commit

--- app/src/main/java/com/example/ian/travelsafe/MapsActivity.java
+++ app/src/main/java/com/example/ian/travelsafe/MapsActivity.java
@@ -43,4 +43,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
+
+    void test() {}
+
 }
