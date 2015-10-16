58956b920a7c7d79dc088b811df15439c6c28834
Thomas Moran <thomaslmoran.14@gmail.com>
2015-10-16 21:15:50 +0100

    End of day 16/10/2015

--- app/src/main/java/com/example/ian/travelsafe/LoginActivity.java
+++ app/src/main/java/com/example/ian/travelsafe/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
 import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 
@@ -15,6 +16,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_login);
     }
 
