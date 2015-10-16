761f355bf5744990f1ecbf73cfa33c1658539f5c
Thomas Moran <thomaslmoran.14@gmail.com>
2015-10-16 22:01:02 +0100

    End of day 16/10/2015

--- app/src/main/java/com/example/ian/travelsafe/AppLoadScreen.java
+++ app/src/main/java/com/example/ian/travelsafe/AppLoadScreen.java
@@ -4,13 +4,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class AppLoadScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //Remove title bar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        //Remove notification bar
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //set content view AFTER ABOVE sequence (to avoid crash)
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_app_load_screen);
+        this.setContentView(R.layout.activity_app_load_screen);
     }
 
     @Override
